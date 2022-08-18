a195117c02ce98bc71748f5776d14cec98c00aea
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-17 21:34:49 -0700

    building page architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,27 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SplashScreenStateService } from './splash-screen-state.service';
 import { SplashComponent } from './components/splash/splash.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    SplashComponent
+    SplashComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
