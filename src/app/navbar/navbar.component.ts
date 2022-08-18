1f9fae60c5dbbf1e0aac688fba19f4030157fd5b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-18 13:14:36 -0700

    adding timer to website

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   mouseHoverEvent: boolean = false;
-  constructor() { }
+  public now: Date = new Date();
+  hours: any;
+  minutes: any;
+  seconds: any;
+  dayTime: string = '';
+  constructor() {
+    setInterval(() => {
+      this.now = new Date();
+      this.hours = this.now.getHours()
+      this.minutes = this.now.getMinutes();
+      this.seconds = this.now.getSeconds();
+      if (this.hours > 12) {
+        this.dayTime = 'PM';
+        this.hours = this.hours - 12;
+      } else {
+        this.dayTime = 'AM';
+      }
+
+    }, 500)
+    console.log(this.now);
+  }
 
   ngOnInit(): void {
   }
