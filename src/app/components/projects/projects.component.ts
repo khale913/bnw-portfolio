82c6e0a40ea7852a85223571c0e957bfd799708d
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-08-22 17:16:36 -0700

    adding hover affects to projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+  mouseHoverProjectOne: boolean = false;
+  mouseHoverProjectTwo: boolean = false;
+  mouseHoverProjectThree: boolean = false;
+  mouseHoverProjectFour: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hoverFunction(project: any, bool: boolean) {
+    if (project === 'one') {
+      this.mouseHoverProjectOne = bool;
+    }
+    if (project === 'two') {
+      this.mouseHoverProjectTwo = bool;
+    }
+    if (project === 'three') {
+      this.mouseHoverProjectThree = bool;
+    }
+    if (project === 'four') {
+      this.mouseHoverProjectFour = bool;
+    }
   }
 
 }
